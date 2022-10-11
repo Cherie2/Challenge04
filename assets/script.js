151b9c3c212f22258d53a08b4e6dc7c4b79c29fc
Cherie2 <cwalk280@yahoo.com>
2022-10-10 20:57:54 -0700

    addition of comments and revision of css, html btns, and js functions

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-
+//Array of objects that include questions,choices, and answers 
 var questions = [
     {
     question: "What do Javascript file extensions end with?",
@@ -31,7 +31,7 @@ var questions = [
     answer:"B.) Boolean"
     }
 ]
-
+//variables that allow functions to track where in index certain outputs will be
 var startButton = document.querySelector("#quiz-btn");
 var quiz = document.querySelector("#quiz-question");
 var choices = document.querySelector("#choices");
@@ -41,26 +41,25 @@ var resultsE = document.querySelector("#results-btn");
 var intialsE = document.querySelector("#endscreen");
 
 var currentQuestionIndex = 0;
-var results= 0;
-
+var results= "";
+//when start is clicked, timer and generateQuestion function is initiated
 function startQuiz () {
     countDown();
     generateQuestion();
 }
-
+//Generates question and answers from var questions
 function generateQuestion(){
-
-   if(currentQuestionIndex >= questions.length){
+//Ends the generate function if all questions have been cycled through    
+    if(currentQuestionIndex >= questions.length){
         endScreen ();
         } else {
             quiz.innerHTML="";
             choices.innerHTML="";
-    
             var showQuestion = questions[currentQuestionIndex];
             var h1 = document.createElement("h1");
             h1.textContent = showQuestion.question;
             quiz.append(h1);
-   
+ //creates li element for answers  
             for(var i = 0; i < showQuestion.answers.length; i++){
                 var liE = document.createElement("li");
                 liE.textContent = showQuestion.answers[i];
@@ -70,45 +69,33 @@ function generateQuestion(){
 }
 
 function checkAnswer(event){
-    
+ //creates h1 element after user selects answer, and informs user if they are correct/incorrect, also tallies correct answers   
     if(questions[currentQuestionIndex].answer == event.target.innerText){
-        resultsE.innerHTML="";
         results++;
-        var resultsEl=document.createElement("h2");
-        resultsEl.textContent=results;
         confirmE.innerHTML="";
         var confirm = document.createElement("h1");
         confirm.textContent ="Correct";
         confirmE.append(confirm);
-        
     } else {
         confirmE.innerHTML="";
         var confirm = document.createElement("h1");
         confirm.textContent ="Incorrect";
         confirmE.append(confirm);
-        
         }
+//Allows user to answer next question by cycling through index and generating next question        
       currentQuestionIndex++;
       generateQuestion ();
 }
-
+//timer function
 function countDown () {
-    var timeLeft = 60;
+    var timeLeft = 20;
     var timeInterval = setInterval(function () {
-        if(timeLeft > 1) {
+        if(timeLeft >= 0) {
          timer.textContent = timeLeft + " seconds remaining";
          timeLeft--;
-        } else if (timeLeft === 1) {
-         timer.textContent = timeLeft + " seconds remaining";
-         timeLeft--
-        } else {
-         timer.textContent = "";
-          clearInterval(timeInterval);
-          endScreen();
-        }
-       }, 1000)
-     }
-
+        }}, 1000)
+    }
+//when timer reaches 0 or all questions have been answered, end form appears for users to submit their initials 
 function endScreen () {
     quiz.innerHTML="";
     choices.innerHTML="";
@@ -116,14 +103,16 @@ function endScreen () {
     end.textContent="You are finished";
     intialsE.append(end);
     var initials = document.createElement("input");
+    initials.setAttribute("style", "background-color: darkgoldenrod; color:black");
     intialsE.append(initials);
     var submit=document.createElement("button");
-    submit.textContent="submit initials";
+    submit.textContent="Submit Initials";
     intialsE.append(submit);
+    submit.setAttribute("style", "background-color: darkgoldenrod; color: darkred");
 }
-
+//functions that allow user to save and view score
 function saveScore (event){
-    if(event.target.textContent == "submit initials"){
+    if(event.target.textContent == "Submit Initials"){
         var score = document.querySelector("input");
         score.textContent="";
         localStorage.setItem("High Scores", JSON.stringify(`Initials: ${score.value}, Score: ${results}`));
@@ -138,10 +127,20 @@ function viewScore () {
     resultsE.append(results);
     }   
 }
+//allows user to see last score recorded
+function viewHighScore () {
+    function viewScore () {
+        score = "";
+        var results = document.createElement("h1");
+        results = localStorage.getItem("High Scores", (`Initials: ${score}, Score: ${results}`));
+        results.textContent=results;
+        resultsE.append(results);
+        }  
+        viewScore();
+}
 
-
-
+//event listeners added for functions to start quiz app
 startButton.addEventListener('click', startQuiz);
 choices.addEventListener("click", checkAnswer);
 intialsE.addEventListener("click", saveScore);
-resultsE.addEventListener("click", viewScore);
+resultsE.addEventListener("click", viewHighScore);
