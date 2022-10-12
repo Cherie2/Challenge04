0aefe5fd8f225018e6b85eb452d60cfd973d7b87
Cherie2 <cwalk280@yahoo.com>
2022-10-12 00:36:18 -0700

    revised JS to include deduct fx and start over fx

--- assets/script.js
+++ assets/script.js
@@ -31,26 +31,29 @@ var questions = [
     answer:"B.) Boolean"
     }
 ]
-//variables that allow functions to track where in index certain outputs will be
+//variables that allow functions to track where in the index certain outputs will be
 var startButton = document.querySelector("#quiz-btn");
 var quiz = document.querySelector("#quiz-question");
 var choices = document.querySelector("#choices");
 var confirmE = document.querySelector("#confirm");
 var timeLeft = document.querySelector("#timer")
-var resultsE = document.querySelector("#results-btn");
+var resultsE = document.querySelector("#results");
 var intialsE = document.querySelector("#endscreen");
+var resultsBtn = document.querySelector("#results-btn");
+var startOver = document.querySelector("#start-over")
 
 var currentQuestionIndex = 0;
-var results= "";
+var results= 0;
+var timeLeft=40;
 
-//when start is clicked, timer and generateQuestion function is initiated
+//when start is clicked, countdown and generateQuestion function is initiated
 function startQuiz () {
     countDown();
     generateQuestion();
 }
-//Generates question and answers from var questions
+//Generates question and answers from variable questions
 function generateQuestion(){
-//Ends the generate function if all questions have been cycled through    
+//Ends the generate function and calls the endscreen function, if all questions have been cycled through    
     if(currentQuestionIndex >= questions.length){
         endScreen ();
         } else {
@@ -60,7 +63,7 @@ function generateQuestion(){
             var h1 = document.createElement("h1");
             h1.textContent = showQuestion.question;
             quiz.append(h1);
- //creates li element for answers  
+ //creates li element for answers to be displayed in 
             for(var i = 0; i < showQuestion.answers.length; i++){
                 var liE = document.createElement("li");
                 liE.textContent = showQuestion.answers[i];
@@ -68,7 +71,7 @@ function generateQuestion(){
              }
         }
 }
-
+//function that checks answers selected by user and returns correct/incorrect feedback
 function checkAnswer(event){
  //creates h1 element after user selects answer, and informs user if they are correct/incorrect, also tallies correct answers   
     if(questions[currentQuestionIndex].answer == event.target.innerText){
@@ -82,66 +85,93 @@ function checkAnswer(event){
         var confirm = document.createElement("h1");
         confirm.textContent ="Incorrect";
         confirmE.append(confirm);
+        deduct();
         }
-//Allows user to answer next question by cycling through index and generating next question        
+//Allows user to answer next question by cycling through index and generating next question 
       currentQuestionIndex++;
       generateQuestion ();
 }
-//timer function
+//Function that deducts time for incorrect answers
+ function deduct(event){ 
+            if(confirm.textContent ="Incorrect"){
+            timeLeft -= 5;
+            timer.textContent = timeLeft + " seconds remaining";
+            timeLeft--;
+            }
+        }
+//timer function for starting countdown and calls endscreen function when it hits 0
 function countDown () {
-    var timeLeft = 20;
     var timeInterval = setInterval(function () {
         if(timeLeft >= 0) {
          timer.textContent = timeLeft + " seconds remaining";
          timeLeft--;
-        }}, 1000)
+        }
+       if(timeLeft === 0 && (currentQuestionIndex >= questions.length)) {
+            clearInterval(timeInterval);
+            timer.setAttribute("style", "display:none");
+          }else if (timeLeft === 0) {
+                clearInterval(timeInterval);
+                endScreen();
+                timer.setAttribute("style", "display:none");
+          }}, 1000)
     }
-//when timer reaches 0 or all questions have been answered, end form appears for users to submit their initials 
+//Function for end form that allows users to submit their initials 
 function endScreen () {
     quiz.innerHTML="";
     choices.innerHTML="";
     var end = document.createElement("h1");
-    end.textContent="You are finished";
+    end.textContent="QUIZ IS OVER!";
     intialsE.append(end);
+    end.setAttribute("style", "text-align: center; font-size: 5rem")
     var initials = document.createElement("input");
-    initials.setAttribute("style", "background-color: darkgoldenrod; color:black");
+    initials.setAttribute("style","background-color: darkgoldenrod; color:darkred");
     intialsE.append(initials);
     var submit=document.createElement("button");
     submit.textContent="Submit Initials";
     intialsE.append(submit);
     submit.setAttribute("style", "background-color: darkgoldenrod; color: darkred");
+    takeAgain();
 }
+
 //functions that allow user to save and view score
 function saveScore (event){
     if(event.target.textContent == "Submit Initials"){
         var score = document.querySelector("input");
         score.textContent="";
-        localStorage.setItem("High Scores", JSON.stringify(`Initials: ${score.value}, Score: ${results}`));
-        localStorage.getItem("High Scores", (`Initials: ${score.value}, Score: ${results}`));
+        localStorage.setItem("High Scores", JSON.stringify(`Name: ${score.value}, Score: ${results}`));
+        localStorage.getItem("High Scores", (`Name: ${score.value}, Score: ${results}`));
         viewScore();
     }
 
 function viewScore () {
     var results = document.createElement("h1");
-    results = localStorage.getItem("High Scores", (`Initials: ${score}, Score: ${results}`));
+    results = localStorage.getItem("High Scores", (`Name: ${score}, Score: ${results}`));
     results.textContent=results;
     resultsE.append(results);
     }   
 }
 //allows user to see last score recorded
-function viewHighScore () {
-    function viewScore () {
-        score = "";
-        var results = document.createElement("h1");
-        results = localStorage.getItem("High Scores", (`Initials: ${score}, Score: ${results}`));
-        results.textContent=results;
-        resultsE.append(results);
-        }  
-        viewScore();
+function viewHighScore (event) {
+        var highScores = document.createElement("h2");
+        highScores = localStorage.getItem("High Scores");
+        highScores.value=highScores;
+        resultsBtn.append(highScores);
+        
+    }
+          
+       
+function takeAgain (event){
+        var take = document.createElement("button");
+        take.textContent ="START OVER";
+        startOver.append(take);
+        take.setAttribute("style", "background-color: darkgoldenrod; color:darkred");
+        if(event.target.click){
+            window.location.reload("Refresh");
+        }
 }
-
-//event listeners added for functions to start quiz app
+//event listeners added for functions to conduct quiz app
 startButton.addEventListener('click', startQuiz);
 choices.addEventListener("click", checkAnswer);
 intialsE.addEventListener("click", saveScore);
-resultsE.addEventListener("click", viewHighScore);
+resultsBtn.addEventListener("click", viewHighScore);
+startOver.addEventListener("click", takeAgain);
